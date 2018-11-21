c93e95565b61eab0f07e6c629969c98baecf4623
isuzuki <ikkuntech@gmail.com>
2018-11-21 15:04:56 +0900

    added eslint

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
 
-exports.handler = async (event) => {
+exports.handler = async (event, context, callback) => {
     console.log(event);
+    console.log(context);
+    console.log(callback);
     return 'Hello index js lambda';
 };
